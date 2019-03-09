5b7cf95315986cb41ed5bacfd722961dffd3d395
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-03-09 12:08:16 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+void main() 
+{
+	int n,a[30],i,j,temp=0,c;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		c=1;
+	    for(j=i+1;j<n;j++)
+	    {
+			if(a[i]==a[j])
+			c++;
+	    }
+	    if(c>temp)
+		    {
+			    temp=c;
+		    }
+	}
+	printf("%d",temp);
+}
